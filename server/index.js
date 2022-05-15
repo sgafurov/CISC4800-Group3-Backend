50f03979f451a495b96491f76d10cf7298a14da1
Shakhram <shakag6@gmail.com>
2022-05-15 11:07:41 -0400

    add app.use cors headers

--- server/index.js
+++ server/index.js
@@ -17,6 +17,16 @@ app.use(bodyParser.urlencoded({ extended: true })); //true or false?
 app.use(bodyParser.json());
 // app.use('/api', require('./api/index'))
 
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+
 app.use('/', require('./api/user'))
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', '*');
