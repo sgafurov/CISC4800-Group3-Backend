d79ce97ab775262143a6d6f883be77c7e044e15d
Shakhram <shakag6@gmail.com>
2022-05-24 16:22:02 -0400

    Add host env variable

--- server/db/database.js
+++ server/db/database.js
@@ -2,9 +2,8 @@ const pkg = require('../../package.json')
 require('dotenv').config()
 
 const Sequelize = require('sequelize');
-const DBURL = "postgres://vikduugkssveon:2c9b6a913355e40012246ebd6d3e129d171c68f71891a5fdb68f4478efd6f4fd@ec2-3-218-171-44.compute-1.amazonaws.com:5432/d5b93skn88ug65";
 const db_url = process.env.DATABASE_URL
-console.log('process.env.DATABASE_URL -->', process.env.DATABASE_URL)
+const db_host = process.env.HOST
 
 /*
 Host
@@ -17,14 +16,14 @@ URI postgres://njweyjexkafpcb:00c6b9ac71dd4ab3f9e7fbab1f0b16444d1fcaebf466575d5c
 Heroku CLI heroku pg:psql postgresql-transparent-29164 --app postgresrmatest
 */
 
-const db = new Sequelize('postgres://vikduugkssveon:2c9b6a913355e40012246ebd6d3e129d171c68f71891a5fdb68f4478efd6f4fd@ec2-3-218-171-44.compute-1.amazonaws.com:5432/d5b93skn88ug65', {
-  host: 'ec2-3-218-171-44.compute-1.amazonaws.com',
+const db = new Sequelize(db_url, {
+  host: db_host,
   port: 5432,
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
     ssl: {
-      rejectUnauthorized: false //false or truew??
+      rejectUnauthorized: false //false or true?
     },
   },
   pool: {
